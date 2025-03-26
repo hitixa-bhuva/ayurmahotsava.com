09c2894ece0a47bad45731bc5d671954ac58a3ff
patel-hitixa <hitixa.bhuva@theuniqueitsolution.com>
2025-03-26 16:20:33 +0530

    change home page hero, nav first logo,  Registration  btn, header font size,

--- _c/js/dev-script-v1.js
+++ _c/js/dev-script-v1.js
@@ -1,5 +1,5 @@
 function redirectToWhatsApp() {
-    var phoneNumber = "+918239677909"; // No extra space at the end
+    var phoneNumber = "+917208628784"; // No extra space at the end
     var message = ""; // Optional message
     var url =
       "https://api.whatsapp.com/send?phone=" +
